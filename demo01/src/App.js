bae09cc049a734fa1e3b0daaa093d67bca4f5eeb
lhubel <jkl5914@naver.com>
2024-09-12 10:51:38 +0900

    수정완료

--- demo01/src/App.js
+++ demo01/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+//React의 화면 파일의 구조
+//1. import를 통해 필요한 요소를 불러온다
+//2. function을 만들어서 화면을 반환하도록 구성한다
+//3. export를 통해서 외부에서 화면을 가져갈 수 있도록 설정한다.
 
-function App() {
+
+// function App(){
+//   return 화면;
+// }
+// export default App;
+
+const App = () => {
+
+  // 반환하는 화면은 JSX 형식으로 구성해야 한다
+  // - 반드시 최상위 태그는 한 개여야 한다
+  // - 여러 개의 태그를 쓰면서 태그 낭비를 하기 싫다면 Fragment 생성
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <h1>리액트 준비 완료!</h1>
+  </>
   );
 }
-
 export default App;
+
+// export default ()=>{
+//   return 화면;
+// };
