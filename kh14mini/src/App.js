0b83b63bbbbe5bee17b0b0abbc452a8dc3a7defb
mincheol11 <f438146@gmail.com>
2024-09-30 21:53:28 +0900

    approval 폴더 추가,기안서 디자인 수정중

--- kh14mini/src/App.js
+++ kh14mini/src/App.js
@@ -4,6 +4,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import Footer from './components/Footer/Footer';
 import Chat from "./components/Chat/Chat";
+import ApprovalForm from "./components/Approval/ApprovalForm";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" exact element={<LoginPage />} /> {/* 로그인 페이지 */}
         <Route path="/main" element={<MainPage />} /> {/* 메인 페이지 */}
         <Route path="/chat" element={<Chat/>}/> {/*채팅페이지*/}
+        <Route path="/approval" element={<ApprovalForm/>}/> {/*채팅페이지*/}
       </Routes>
       <Footer />
     </HashRouter>
