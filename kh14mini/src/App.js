b8f2b8dbfaa3dbde90d0d24917a6ed98e6ec13c5
minseokOo <minseok916@gmail.com>
2024-09-30 18:18:45 +0900

    채팅 병합중

--- kh14mini/src/App.js
+++ kh14mini/src/App.js
@@ -3,6 +3,7 @@ import MainPage from './components/Main/MainPage';
 import { HashRouter, Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import Footer from './components/Footer/Footer';
+import Chat from "./components/Chat/Chat";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" exact element={<LoginPage />} /> {/* 로그인 페이지 */}
         <Route path="/main" element={<MainPage />} /> {/* 메인 페이지 */}
+        <Route path="/chat" element={<Chat/>}/> {/*채팅페이지*/}
       </Routes>
       <Footer />
     </HashRouter>
---
+++ kh14mini/src/components/Chat/ChatroomList.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const ChatroomList = ()=>{
+    
+    const [chatroomList, setChatroomList] = useState([]);
+    return (<>
+        채팅창 리스트
+    </>);
+}
+
+export default ChatroomList;
