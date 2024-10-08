e32a574f289e64049bc72d19a5c5f1382d461e82
mincheol22 <jumsoon0043@gmail.com>
2024-10-08 21:44:56 +0900

    app js 수정

--- kh14mini/src/App.js
+++ kh14mini/src/App.js
@@ -1,6 +1,6 @@
 import LoginPage from './components/Login/LoginPage';
 import MainPage from './components/Main/MainPage';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import NotFoundPage from './components/NotFound/NotFoundPage';
@@ -18,40 +18,37 @@ import CorporateSealRequestForm from "./components/Approval/CorporateSealRequest
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import DocumentList from './components/Approval/DocumentList';
 import DocumentDetail from './components/Approval/DocumentDetail';
-import Meeting from './components/Meeting/Meeting';
-
 
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      
-      {/* PrivateRoute를 사용하는 부분 */}
-      <Route element={<PrivateRoute />}>
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/meeting" element={<Meeting />} />
-      </Route>
-      
-      <Route path="/test/list" element={<DocumentList />} />
-      <Route path="/test/detail" element={<DocumentDetail />} />
-      <Route path="/test1" element={<EmploymentContractForm />} /> 
-      <Route path="/test2" element={< ResignationForm />} /> 
-      <Route path="/test3" element={<EquipmentManagementForm />} /> 
-      <Route path="/test4" element={<BranchTransferForm />} /> 
-      <Route path="/test5" element={<LeaveApplicationForm />} /> 
-      <Route path="/test6" element={<PregnancyWorkReductionForm />} /> 
-      <Route path="/test7" element={<ChildcareLeaveApplicationForm />} /> 
-      <Route path="/test8" element={<StaffTrainingApplicationForm />} /> 
-      <Route path="/test9" element={<OrderDraftForm />} /> 
-      <Route path="/test10" element={<CorporateSealRequestForm />} /> 
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        
+        {/* PrivateRoute를 사용하는 부분 */}
+        <Route element={<PrivateRoute />}>
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/chat" element={<Chat />} />
+        </Route>
 
+        <Route path="/test/list" element={<DocumentList />} />
+        <Route path="/test/detail" element={<DocumentDetail />} />
+        <Route path="/employment-contract/detail" element={<EmploymentContractForm />} />
+        <Route path="/resignation/detail" element={<ResignationForm />} />
+        <Route path="/inventory-supply/detail" element={<EquipmentManagementForm />} />
+        <Route path="/branch-transfer/detail" element={<BranchTransferForm />} />
+        <Route path="/leave-application/detail" element={<LeaveApplicationForm />} />
+        <Route path="/pregnancy-work-reduction/detail" element={<PregnancyWorkReductionForm />} />
+        <Route path="/childcare-leave/detail" element={<ChildcareLeaveApplicationForm />} />
+        <Route path="/staff-training/detail" element={<StaffTrainingApplicationForm />} />
+        <Route path="/order-draft/detail" element={<OrderDraftForm />} />
+        <Route path="/corporate-seal/detail" element={<CorporateSealRequestForm />} />
 
-      {/* 페이지가 없으면 보여줄 페이지 - 404 Not Found */}
-      <Route path="*" element={<NotFoundPage />} />
-
-    </Routes>
+        {/* 페이지가 없으면 보여줄 페이지 - 404 Not Found */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 };
 
